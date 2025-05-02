9c81901ab446730cce9dc53c08728a75b08eb39d
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-05-02 15:40:29 +0500

    feat: backend admin first commit

---
+++ src/api/app.service.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { CustomLogger } from '../infrastructure/lib/custom-logger/logger.service';
+import * as path from 'path';
+import * as express from 'express';
+// import { AllExceptionsFilter } from '../infrastructure';
+// import { config } from '../config';
+
+export default class Application {
+  public static async main(): Promise<void> {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({ origin: '*' });
+    const logger = app.get(CustomLogger);
+    // app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      }),
+    );
+    app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+    const api = 'api/v1';
+    const swaggerApi = 'api/docs';
+    app.setGlobalPrefix(api);
+    const config_swagger = new DocumentBuilder()
+      .setTitle('LMS')
+      .setVersion('1.0')
+      .addBearerAuth({
+        type: 'http',
+        scheme: 'Bearer',
+        in: 'Header',
+      })
+      .build();
+    const documentFactory = () =>
+      SwaggerModule.createDocument(app, config_swagger);
+    SwaggerModule.setup(swaggerApi, app, documentFactory);
+    const server_port = process.env.API_PORT ?? 3000;
+    await app.listen(server_port, () => {
+      logger.log(`Server: http://localhost:${server_port}/api/v1/`);
+    });
+  }
+}
